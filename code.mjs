9ca4538b3e645186fb75cb2714a1ced7b03b7e2b
Matteo Bucciol <62759388+matteobu@users.noreply.github.com>
2024-12-02 12:23:49 +0100

    Day 01 and Day 02

---
+++ code.mjs
@@ -0,0 +1,50 @@
+import fs from 'fs';
+
+// DAY 01
+const col1 = [];
+const col2 = [];
+const diffCol = [];
+
+function getFileLines(fileName) {
+  const columns = fs.readFileSync(fileName, 'utf-8');
+  return columns.split('\n');
+}
+getFileLines('data.txt').forEach((item) => {
+  const [first, second] = item.split('   ');
+  col1.push(+first);
+  col2.push(+second);
+});
+
+const ascendCol1 = col1.sort();
+const ascendCol2 = col2.sort();
+for (let i = 0; i < col1.length; i++) {
+  diffCol.push(Math.abs(ascendCol1[i] - ascendCol2[i]));
+}
+const totalSum = diffCol.reduce(
+  (accumulator, currentValue) => accumulator + currentValue,
+  0
+);
+console.log(totalSum); // 1579939
+
+// DAY 02
+const multiCol = [];
+
+// col1
+for (let i = 0; i < ascendCol1.length; i++) {
+  ascendCol2.forEach((aC2) => {
+    if (aC2 === ascendCol1[i]) {
+      multiCol.push(ascendCol1[i]);
+    }
+  });
+}
+
+const countMap = multiCol.reduce((acc, num) => {
+  acc[num] = (acc[num] || 0) + 1; // Increment count for each number
+  return acc;
+}, {});
+
+const totalMultiSum = Object.entries(countMap).reduce((sum, [num, count]) => {
+  return sum + num * count;
+}, 0);
+
+console.log(totalMultiSum); // 20351745
