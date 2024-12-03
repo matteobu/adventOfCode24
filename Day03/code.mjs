659ef775e3d3d3d2eed842bc20701e4d36636017
Matteo Bucciol <62759388+matteobu@users.noreply.github.com>
2024-12-03 11:07:49 +0100

    Day 03 complete

--- Day03/code.mjs
+++ Day03/code.mjs
@@ -1,15 +1,39 @@
 import fs from 'fs';
 
-const data = fs.readFileSync('data.txt', 'utf-8');
-const regex = /mul\((\d{1,3}),(\d{1,3})\)/g;
+const inputString = fs.readFileSync('data.txt', 'utf-8');
+const mulRegex = /mul\((\d{1,3}),(\d{1,3})\)/g;
 
+// // DAY 03 PART 01
 const matches = [];
-let match;
-while ((match = regex.exec(data)) !== null) {
-  matches.push([parseInt(match[1], 10), parseInt(match[2], 10)]);
+let mulMatch;
+while ((mulMatch = mulRegex.exec(inputString)) !== null) {
+  matches.push([parseInt(mulMatch[1], 10), parseInt(mulMatch[2], 10)]);
 }
 
 const results = matches.map(([x, y]) => x * y);
-const totalSum = results.reduce((sum, value) => sum + value, 0);
+const totalMulSum = results.reduce((sum, value) => sum + value, 0);
 
-console.log(totalSum); // 161
+console.log(totalMulSum); // 174561379
+
+// DAY 03 PART 02
+const combinedRegex = /(do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\))/g;
+
+let isEnabled = true;
+let totalSum = 0;
+let match;
+
+while ((match = combinedRegex.exec(inputString)) !== null) {
+  const [instruction, x, y] = match;
+
+  if (instruction === 'do()') {
+    isEnabled = true;
+  } else if (instruction === "don't()") {
+    isEnabled = false;
+  } else if (x !== undefined && y !== undefined) {
+    if (isEnabled) {
+      totalSum += parseInt(x, 10) * parseInt(y, 10);
+    }
+  }
+}
+
+console.log(totalSum); //  106921067
